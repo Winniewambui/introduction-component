cf0b6077b6340bd312eebf6bb29c917c92ce3ff2
winniewambui <winniebwambui7@gmail.com>
2023-05-03 11:24:46 +0300

    second commit

--- app.js
+++ app.js
@@ -1,2 +1,28 @@
+const firstNameInput = document.getElementById('first_name');
+const lastNameInput = document.getElementById('last_name');
+const emailInput = document.getElementById('email');
+const passwordInput = document.getElementById('password');
+
+const addForm = document.forms['form'];
+addForm.addEventListener('submit', component);
+
+function component (e) {
+    e.preventDefault();
+
+    const cautionMsg = document.querySelector('.hide');
+    if (
+        firstNameInput.value === '' ||
+        lastNameInput.value === '' ||
+        emailInput.value === '' ||
+        passwordInput.value === ''
+    ) {
+        cautionMsg.style.display = 'inline';
+        // cautionMsg.style.height =`${ cautionMsg.scrollHeight}px`;
+    } else {
+        cautionMsg.style.height = '0px';
+    }
+}
+    
+
 
 
