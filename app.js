265f880aee4fa2aae6d411b29bd400fc76107ccc
winniewambui <winniebwambui7@gmail.com>
2023-05-12 12:34:50 +0300

    fix bugs

--- app.js
+++ app.js
@@ -1,28 +1,85 @@
-const firstNameInput = document.getElementById('first_name');
-const lastNameInput = document.getElementById('last_name');
-const emailInput = document.getElementById('email');
-const passwordInput = document.getElementById('password');
+const firstNameValue = document.getElementById('first_name').value;
+const lastNameValue = document.getElementById('last_name').value;
+const emailValue = document.getElementById('email').value;
+const passwordValue = document.getElementById('password').value;
+const button = document.getElementById('button-btn');
 
-const addForm = document.forms['form'];
-addForm.addEventListener('submit', component);
+// const form = document.forms['form'];
 
-function component (e) {
+button .addEventListener('click', checkInput);
+
+function checkInput (e){
     e.preventDefault();
 
-    const cautionMsg = document.querySelector('.hide');
-    if (
-        firstNameInput.value === '' ||
-        lastNameInput.value === '' ||
-        emailInput.value === '' ||
-        passwordInput.value === ''
-    ) {
-        cautionMsg.style.display = 'inline';
-        // cautionMsg.style.height =`${ cautionMsg.scrollHeight}px`;
-    } else {
-        cautionMsg.style.height = '0px';
-    }
+    // const firstNameValue = firstName.value.trim();
+    // const lastNameValue = lastName.value.trim();
+    // const emailValue = email.value.trim();
+    // const passwordValue = password.value.trim();
+
+if(firstNameValue === ''){
+    setErrorFor(firstName, 'First name cannot be empty');
+}else{
+    setSUccessFor(firstName);
 }
-    
+if (lastNameValue === '') {
+    setErrorFor(lastName, 'Last name cannot be empty');
+} else {
+    setSuccessFor(lastName);
+}
+if (passwordValue === '') {
+    setErrorFor(password, 'Password cannot be empty');
+} else if (passwordValue.length < 8) {
+    setErrorFor(password, 'Password must be at least 8 characters long');
+} else {
+    setSuccessFor(password);
+}
+
+function setErrorFor(input, message){
+     const formControl = input.parent;
+     const small= formControl.querySelector('small');
+
+      small.innerText = message;
+
+     formControl.className = 'form-control error';
+}
+function setSuccessFor(input) {
+    const formControl = input.parentNode;
+    formControl.className = 'form-control success';
+}
 
 
 
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+// function error(element, msg){
+// element.style.border = '2px solid red';
+// const parent = element.parentNode;
+// const p = parent.querySelector('#error-message');
+// p.textContent = msg;
+// p.style.visibility = 'visible';
+// };
